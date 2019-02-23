049c4ddc5212e5ade2f578623633ca0088c06e71
Alexandr Ryabtsev <awryabtsev@GMail.com>
2019-02-23 23:02:57 +0300

    Task 3601 done!

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -3,6 +3,8 @@ package com.javarush.task.task37.task3702;
 import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
+import java.util.Iterator;
+
 public class FactoryProducer {
     public static enum HumanFactoryType {MALE, FEMALE};
 
