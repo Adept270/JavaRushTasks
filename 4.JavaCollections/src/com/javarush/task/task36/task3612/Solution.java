8eab7726b5cb15d768bbc586400f5e9a3de06fb8
Alexandr Ryabtsev <awryabtsev@GMail.com>
2020-04-20 08:49:56 +0300

    Task task3612 done!

--- 4.JavaCollections/src/com/javarush/task/task36/task3612/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3612/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task36.task3612;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -34,6 +35,9 @@ public class Solution {
     }
 
     protected void updateLastDate(long delta) {
+        dates.remove(lastDate);
         lastDate.setTime(lastDate.getTime() + delta);
+        dates.add(lastDate);
     }
+
 }
