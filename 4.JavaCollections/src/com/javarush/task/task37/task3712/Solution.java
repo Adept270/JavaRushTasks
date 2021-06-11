cf06a1aa1ce53b86a64cadfa15aa1e7db4b8740a
Alexandr Ryabtsev <awryabtsev@GMail.com>
2021-06-11 09:24:48 +0300

    Task task3713 done!

--- 4.JavaCollections/src/com/javarush/task/task37/task3712/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3712/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task37.task3712;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Шаблонный метод
 */
+
 public class Solution {
     public static void main(String[] args) {
         List<Game> games = new ArrayList<>();
