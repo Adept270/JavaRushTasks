8f203ba600cf5f25c109fc162e0cf658ebcd4a2a
Alexandr Ryabtsev <awryabtsev@GMail.com>
2021-06-16 19:09:12 +0300

    Parser 4 done

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParserTest.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task39.task3913;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class LogParserTest {
+
+
+    @Test
+    public void testFind() throws IOException {
+//        LogParser logParser = new LogParser(Path.of("C:/Users/16727641/Documents/Java/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task39/task3913/logs/"));
+//
+//
+//        System.out.println("============");
+//
+//        logParser.getNumberOfUniqueIPs(new Date(), new Date());
+    }
+}
